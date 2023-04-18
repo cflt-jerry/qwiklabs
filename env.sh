0d9026d3ec0955fa4a14d103f9a0319ffd78420f
Jerry Gilyeat <111791409+cflt-jerry@users.noreply.github.com>
2023-04-18 16:47:04 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# 
+# Basic environment variables export script. Acutal secrets are not included,
+# please create Organization Admin API key to use within this terraform 
+# project.
+#
+# Author: Jerry Gilyeat (jgilyeat@confluent.io)
+# Copyright Confluent, Inc, 2023
 export TF_VAR_user_email=$USER_EMAIL
 export TF_VAR_cloud_api_key=
 export TF_VAR_cloud_api_secret=
