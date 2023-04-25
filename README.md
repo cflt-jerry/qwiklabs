09d79cabd3023c6139814970623629e6b1ca7761
Gerald Gilyeat <ggilyeat@ryg2g29390.lan>
2023-04-25 19:09:42 -0400

    README update gitignore update env.sh update with additional commentary for usage.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# qwiklabs
-Repository for Google Qwiklabs code &amp; coursework documentation. 
+This repository contains the Terraform code to initialize and destroy
+a Google Cloud Skillsboost (Qwiklabs) user and environment. 
+This is to enable programmatic and automatic generation and cleanup
+of training environments within that framework. 
--- env.sh
+++ env.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+##!/bin/bash
+#!/bin/zsh
 # 
-# Basic environment variables export script. Acutal secrets are not included,
+# Basic environment variables export script. Actual secrets are not included,
 # please create Organization Admin API key to use within this terraform 
 # project.
 #
 # Author: Jerry Gilyeat (jgilyeat@confluent.io)
 # Copyright Confluent, Inc, 2023
 export TF_VAR_user_email=$USER_EMAIL
-export TF_VAR_cloud_api_key=
-export TF_VAR_cloud_api_secret=
+
+# Expects the .secrets file to be the downloaded API key from Confluent. 
+# This file is explicitly NOT included in this repository.
+export TF_VAR_cloud_api_key=`grep -A 1 key\: .secrets | grep -v key`
+export TF_VAR_cloud_api_secret=`grep -A 1 secret\: .secrets | grep -v secret`
